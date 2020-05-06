7acdcb357dde59c26735273162a7e0c06e27bb3b
leoAlvTor <calvaradot1@est.ups.edu.ec>
2020-05-05 19:12:50 -0500

    Commit

---
+++ AgendaTelefonica/src/ec/edu/ups/servlets/ServletIMG.java
@@ -0,0 +1,42 @@
+package ec.edu.ups.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletIMG
+ */
+@WebServlet("/ServletIMG")
+public class ServletIMG extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletIMG() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession(false).invalidate();
+		System.out.println("Se invalido la sesion!");
+		response.sendRedirect("/AgendaTelefonica/public/Index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
